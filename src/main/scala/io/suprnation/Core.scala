e72937e3366ee363d888addfde81f103e2aeee4f
Raivis Rimkus <RRimkus@evolutiongaming.com>
2019-08-02 14:04:48 +0300

    Working task, corectly - non formated

--- src/main/scala/io/suprnation/Core.scala
+++ src/main/scala/io/suprnation/Core.scala
@@ -4,13 +4,15 @@ import java.time.LocalDateTime
 
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.collection.mutable
 import scala.io.Source
 
 object Core extends App with LazyLogging {
   logger info s"Core app started at `${LocalDateTime.now}`"
 
   start
-  def start: Unit = {
+
+  def versionThatWasNotCorrect: Unit = {
     val fileContents = Source.fromFile("triangle.txt").getLines().toList
 
     println("### Lets print `triangle.txt` ###")
@@ -28,8 +30,47 @@ object Core extends App with LazyLogging {
 
   }
 
+  def start = {
+    val fileContents = Source.fromFile("triangle.txt").getLines().toList
+    println("### Lets print `triangle.txt` ###")
+    fileContents.foreach(println)
+    println("######")
+    // this time will not sort them to not break logic.
+    val linesToList = fileContents.filterNot(_.isEmpty).map { line =>
+      line.split(" ").toList.map((s: String) => s.toInt)
+    }
+    sum(linesToList)
+  }
 
+  def sum(triangleValues: List[List[Int]]) = {
+    var tempMap = mutable.Map[Int, Int]()
+    var pathMap = mutable.Map[Int, Int]()
+    val minimumSum = triangleValues.reduceRight(
+      (up, low) =>
+        up zip {
+          // case if its first iteration (it should be empty)
+          if(tempMap.nonEmpty) {
+            pathMap += tempMap.toSeq.sorted.head
+            tempMap.clear
+          }
+          low zip low.tail
+        } map {
+          case (above, (left, right)) =>
+            val min = Math.min(left, right)
+            // add temp path to temp path map
+            tempMap += (above+min) ->  min
+            above + Math.min(left, right)
+        }
+    )
+    pathMap += tempMap.toSeq.sorted.head
+    tempMap.clear
+    // map to Seq  + sort
+    val sorted = pathMap.toSeq.sorted.reverse
+    // {Int, Int to Int + add missing element}
+    val dd = sorted.toList.map(x => x._1 - x._2) :+ sorted.last._2
 
-
+    println(s"Minimal path is: ${dd.mkString(" + ")} = ${minimumSum.sum}")
+  }
 
 }
+
